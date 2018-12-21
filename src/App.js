76c98cba5682500f70f66d3d3ae5cd5e4700f606
HarrisonVanNorth <hgvannort@gmail.com>
2018-12-21 13:55:48 -0700

    add dev.pro

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ class App extends Component {
   
   state = {
   
-    products: [
-      { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
-      { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
-      { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
-      { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
-      { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
-      { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
-      { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
-      { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
-      { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
-    ],
+    products: [],
+    // [
+    //   { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+    //   { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+    //   { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+    //   { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+    //   { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+    //   { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+    //   { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+    //   { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+    //   { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+    // ],
     cartItemsList: [
       { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
       { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
       { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
     ]
+  }
+  async componentDidMount() {
+    const productsResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/products`)
+    const productsJSON = await productsResponse.json()
+    this.setState({
+      products: productsJSON.products
+    })
   }
   
   _handleSubmit = (item) => {
